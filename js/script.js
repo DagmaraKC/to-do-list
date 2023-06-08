e182bf2534c48393dc6abc198d1023deb32ee64a
Dagmara Kot-Czerzniewska <kotdagmara1@gmail.com>
2023-06-08 13:58:56 +0200

    adding new tasks and added style to task done

--- js/script.js
+++ js/script.js
@@ -15,16 +15,43 @@
 
         for (const task of tasks) {
             htmlString += `
-    <li>
+    <li 
+    class="form__listItem ${task.done ? "form__listItem--done" : ""}"
+    >
     ${task.content}
     </li>
     `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document
+            .querySelector(".js-newTask")
+            .value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
     init();
 }
